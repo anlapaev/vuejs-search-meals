506545be541fd675ca379ce6a477c6d00d1d55c6
Андрей Лапаев <an.lapaev@proton-group.ru>
2024-09-10 17:04:58 +0300

    Create Home Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import MealList from "../views/MealList.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: Home,
+    },
+    {
+      path: "/letter/:letter",
+      name: "byletter",
+      component: MealList,
     },
   ],
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,28 @@
 <template>
+  <div class="flex flex-col p-8">
     <div>
-        <pre>{{meals}}</pre>
+      <input
+        type="text"
+        class="rounded border-2 border-gray-200 w-full"
+        placeholder="Search for a meal"
+      />
     </div>
+
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link
+        v-for="letter in letters"
+        :key="letter"
+        :to="{ name: 'byletter', params: { letter } }"
+        class="text-xl"
+        >{{ letter }}</router-link
+      >
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { computed } from 'vue'
-import store from '../store/index.js'
+import { computed } from "vue";
+import store from "../store/index.js";
 
-const meals = computed(() => {
-    return store.state.meals
-})
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 </script>
